fb4e67b6439e04a8019df3e7604eff5a34dba647
Morgan Hallgren <morgan.hallgren@gmail.com>
2016-01-06 20:51:09 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,273 @@
+require "sinatra"
+enable :sessions
+enable :protection
+set :protection, except: :session_hijacking
+use Rack::Deflater
+
+
+#Todo
+get "/" do
+  
+  @todos = todos
+  @todos.each do |todo|
+    todo[:edit_id] = false
+  end
+  if @todos.length == 0
+    erb :todo_index_empty
+  else
+    @completed_count = completed.length
+    @active_count = active.length
+    @all_completed = all_completed? @todos
+    @show_footer_and_toggle_all = todos.length > 0
+    erb :todo_index
+  end
+end
+
+get "/?:route?/edit/:id" do
+  
+  @edit_id = params[:id]
+  @route = params[:route]
+  @todos = todos_based_on_route @route
+
+  @todos.each do |todo|
+    todo[:edit_id] = false
+    todo[:edit_id] = true if todo[:id] == @params[:id]
+  end
+
+  if request.xhr?
+    erb :todos
+  else
+    @completed_count = completed.length
+    @active_count = active.length
+    @all_completed = all_completed? @todos
+    @show_footer_and_toggle_all = @todos.length > 0
+    erb :todo_index
+  end
+end
+
+get "/?:route?/todos" do
+  @route = params[:route]
+  @todos = todos_based_on_route @route
+  erb :todos
+end
+
+get "/?:route?/footer" do
+  @completed_count = completed.length
+  @active_count = active.length
+  @route = params[:route]
+  @show_footer_and_toggle_all = todos.length > 0
+  erb :footer
+end
+
+get "/completed" do
+  @todos = completed
+  @route = "completed"
+  @active_count = active.length
+  @show_footer_and_toggle_all = todos.length > 0
+  @completed_count = completed.length
+  @all_completed = all_completed? todos
+  erb :todo_index
+end
+
+get "/active" do
+  @todos = active
+  @route = "active"
+  @active_count = active.length
+  @show_footer_and_toggle_all = todos.length > 0
+  @completed_count = completed.length
+  @all_completed = all_completed? todos
+  erb :todo_index
+end
+
+post "/?:route?/new_todo" do
+  
+  if params[:id] && !params[:id].empty?
+    id = params[:id]
+  else
+    id = rand(36**8).to_s(36)
+  end
+
+  add_todo({:title => params[:title], :completed => false, :id => id})
+  if request.xhr?
+    @route = params[:route]
+    @todos = todos_based_on_route @route
+    erb :todos
+  else
+    redirect "/#{params[:route]}" if params[:route]
+    redirect "/"
+  end
+end
+
+post "/?:route?/destroy/:id" do
+  destroy_todo(params[:id])
+  if request.xhr?
+    @route = params[:route]
+    @todos = todos_based_on_route @route
+    erb :todos
+  else
+    redirect "/#{params[:route]}" if params[:route]
+    redirect "/"
+  end
+end
+
+get "/toggle_all" do
+  @todos = todos
+  @all_completed = all_completed? @todos
+  @show_footer_and_toggle_all = todos.length > 0
+  erb :toggle_all
+end
+
+post "/?:route?/complete/:id" do
+  complete_todo params[:id]
+  if request.xhr?
+    @route = params[:route]
+    @todos = todos_based_on_route @route
+    erb :todos
+  else
+    redirect "/#{params[:route]}" if params[:route]
+    redirect "/"
+  end
+end
+
+post "/?:route?/reactivate/:id" do
+  reactivate_todo params[:id]
+  if request.xhr?
+    @route = params[:route]
+    @todos = todos_based_on_route @route
+    erb :todos
+  else
+    redirect "/#{params[:route]}" if params[:route]
+    redirect "/"
+  end
+end
+
+post "/?:route?/edit/:id" do
+  edit_todo params[:id], params[:title]
+  if request.xhr?
+    @route = params[:route]
+    @todos = todos_based_on_route @route
+    erb :todos
+  else
+    redirect "/#{params[:route]}" if params[:route]
+    redirect "/"
+  end
+end
+
+post "/?:route?/complete_all" do
+  complete_all_todos
+  if request.xhr?
+    @route = params[:route]
+    @all_completed = all_completed? todos
+    @show_footer_and_toggle_all = todos.length > 0
+    erb :toggle_all
+  else
+    redirect "/#{params[:route]}" if params[:route]
+    redirect "/"
+  end
+end
+
+post "/?:route?/reactivate_all" do
+  reactivate_all_todos
+  if request.xhr?
+    @route = params[:route]
+    @all_completed = all_completed? todos
+    @show_footer_and_toggle_all = todos.length > 0
+    erb :toggle_all
+  else
+    redirect "/#{params[:route]}" if params[:route]
+    redirect "/"
+  end
+end
+
+post "/?:route?/clear_completed" do
+  clear_completed_todos
+  if request.xhr?
+    @completed_count = completed.length
+    @active_count = active.length
+    @route = params[:route]
+    @todos = todos_based_on_route @route
+    @show_footer_and_toggle_all = todos.length > 0
+    erb :footer
+  else
+    redirect "/#{params[:route]}" if params[:route]
+    redirect "/"
+  end
+end
+
+
+private
+
+def no_todos
+  []
+end
+
+def completed
+  todos.select { |todo| todo[:completed] }
+end
+
+def active
+  todos.select { |todo| !todo[:completed] }
+end
+
+def todos
+  return session[:todos].map { |t| t } unless session[:todos] == nil
+  return []
+  #[{:text => "not completed", :completed => false}, {:text => "completed", :completed => true}]
+end
+
+def todos_based_on_route route
+  if route == "completed"
+    return completed
+  elsif route == "active"
+    return active
+  else
+    return todos
+  end
+end
+
+def add_todo todo
+  session[:todos] = [] if session[:todos] == nil
+  session[:todos] << todo
+end
+
+def destroy_todo id
+  session[:todos].delete_if { |todo| todo[:id] == id } 
+end
+
+def complete_todo id
+  todo = find_todo_by_id id
+  todo[:completed] = true
+end
+
+def reactivate_todo id
+  todo = find_todo_by_id id
+  todo[:completed] = false
+end
+
+def edit_todo id, title
+  todo = find_todo_by_id id
+  todo[:title] = title
+end
+
+def find_todo_by_id id
+  session[:todos].select { |todo| todo[:id] == id }.first
+end
+
+def all_completed? ts
+  return true if ts.select { |todo| todo[:completed] == false }.empty?
+  false
+end
+
+def complete_all_todos
+  todos.map { |todo| todo[:completed] = true }
+end
+
+def reactivate_all_todos
+  todos.map { |todo| todo[:completed] = false }
+end
+
+def clear_completed_todos
+  puts todos.inspect
+  puts todos.select { |todo| todo[:completed] == true }
+  session[:todos] = todos.delete_if { |todo| todo[:completed] == true }
+end
---
+++ public/todo.js
@@ -0,0 +1,200 @@
+window.onload = function(){
+    bind();
+
+    window.route = window.location.pathname.replace(/\/$/, '');
+    
+    if (!navigator.serviceWorker) {
+      if (history.pushState) {
+        bindPushStateEvent(); 
+      }
+    }
+
+    
+};
+
+var bindPushStateEvent = function(){
+  var elements = getElementsByClassName(document, "route");
+  n = elements.length;
+  for (var i = 0; i < n; i++) {
+    element = elements[i];  
+  
+    if (element.addEventListener) {
+        element.addEventListener("click", reRoute);
+    } else if (element.attachEvent) {
+        element.attachEvent("click", reRoute);
+    }
+  }
+
+  window.onpopstate = function(event) {
+    window.route = event.state.route.replace(/\/$/, '');
+    var customEvent = new CustomEvent('new_route', {bubbles: true, cancelable: true});
+    window.dispatchEvent(customEvent); 
+  };
+};
+
+var reRoute = function(event){
+  window.route = event.target.pathname.replace(/\/$/, '');
+  var stateObj = { route: event.target.pathname };
+  history.pushState(stateObj, event.target.pathname, event.target.pathname);
+  var customEvent = new CustomEvent('new_route', {bubbles: true, cancelable: true});
+  window.dispatchEvent(customEvent);
+  event.preventDefault();
+};
+
+var getElementsByClassName = function(node,classname) {
+  if (node.getElementsByClassName) { // use native implementation if available
+    return node.getElementsByClassName(classname);
+  } else {
+    return (function getElementsByClass(searchClass,node) {
+        if ( node == null )
+          node = document;
+        var classElements = [],
+            els = node.getElementsByTagName("*"),
+            elsLen = els.length,
+            pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)"), i, j;
+
+        for (i = 0, j = 0; i < elsLen; i++) {
+          if ( pattern.test(els[i].className) ) {
+              classElements[j] = els[i];
+              j++;
+          }
+        }
+        return classElements;
+    })(classname, node);
+  }
+};
+
+var bind = function(){
+    var new_todo_form = document.getElementById("form-new_todo");
+    if (new_todo_form.addEventListener) {
+        new_todo_form.addEventListener("submit", addNewTodo);
+    } else if (new_todo_form.attachEvent) {
+        new_todo_form.attachEvent("submit", addNewTodo);
+    }
+
+    if (window.addEventListener) {
+      window.addEventListener("todo-list-update", updatePartials);
+      window.addEventListener("offline-sync-done", updatePartials);
+    } else if (window.attachEvent) {
+      window.attachEvent("todo-list-update", updatePartials);
+      window.attachEvent("offline-sync-done", updatePartials);
+    }
+
+    if (window.addEventListener) {
+        window.addEventListener("new_route", updatePartials);
+    } else if (window.attachEvent) {
+        window.attachEvent("new_route", updatePartials);
+    }
+
+};
+
+var reBind = function(){
+  var new_todo_form = document.getElementById("form-new_todo");
+  if (new_todo_form.removeEventListener) {
+    new_todo_form.removeEventListener("submit", addNewTodo);
+  } else if (new_todo_form.detachEvent) {
+    new_todo_form.detachEvent("submit", addNewTodo);
+  }
+
+  if (window.removeEventListener) {
+    window.removeEventListener("todo-list-update", updatePartials);
+    window.removeEventListener("offline-sync-done", updatePartials);
+  } else if (window.detachEvent) {
+    window.detachEvent("todo-list-update", updatePartials);
+    window.detachEvent("offline-sync-done", updatePartials);
+  }
+
+  if (window.removeEventListener) {
+    window.removeEventListener("new_route", updatePartials);
+  } else if (window.detachEvent) {
+    window.detachEvent("new_route", updatePartials);
+  }
+  bind();
+  bindPushStateEvent();
+}
+
+var updatePartials = function(event) {
+    get(window.route+"/todos", function(html) {
+      var todo_list = document.getElementById("todo-list");
+      todo_list.innerHTML = html;
+      reBind();
+    });
+
+    get("/toggle_all", function(html) {
+      var toggle_all = document.getElementById("toggle_all");
+      toggle_all.innerHTML = html;
+      reBind();
+    });
+
+    get(window.route+"/footer", function(html) {
+      var foot = document.getElementById("foot");
+      foot.innerHTML = html;
+      reBind();
+    });
+    event.preventDefault();
+};
+
+var addNewTodo = function(event) {
+    event.preventDefault();
+    var href = event.currentTarget.action;
+    var method = event.currentTarget.method;
+    var value = event.target[0].value;
+    var id = event.currentTarget.id;
+    
+    queue.push({"href": href, "method": method, "data": {"value": value}, "id": "", "onlineAction": "addNewTodo"});
+    postDataFromQueueOnline();
+    //var customEvent = new CustomEvent('post-data', {bubbles: true, cancelable: true});
+    //window.dispatchEvent(customEvent);
+
+    // post(href, "title="+event.target[0].value, function(html) {
+    //   var customEvent = new CustomEvent('todo-list-update', {bubbles: true, cancelable: true});
+    //   window.dispatchEvent(customEvent);
+      
+    // });
+    event.target[0].value = "";
+    return false
+};
+
+var get = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+      var DONE = 4; // readyState 4 means the request is done.
+      var OK = 200; // status 200 is a successful return.
+      if (xhr.readyState === DONE) {
+        if (xhr.status === OK) {
+          callback(xhr.responseText);
+          reBind();
+        } else {
+          console.log('Error: ' + xhr.status); // An error occurred during the request.
+        }
+      }
+    };
+};
+
+var post = function(url, data, callbackOk, callbackError) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xhr.send(data);
+
+    xhr.onreadystatechange = function () {
+      var DONE = 4; // readyState 4 means the request is done.
+      var OK = 200; // status 200 is a successful return.
+      if (xhr.readyState === DONE) {
+        if (xhr.status === OK) {
+          callbackOk(xhr.responseText);
+          reBind();
+        } else {
+          callbackError(xhr.status);
+          console.log('Error: ' + xhr.status); // An error occurred during the request.
+        }
+      }
+    };
+};
